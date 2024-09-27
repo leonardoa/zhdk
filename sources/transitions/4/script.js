e16dbe8936b32f631180827179ba3f986a8c86c2
Leonardo Angelucci <mail@leonardo-angelucci.ch>
2024-09-27 15:51:03 +0200

    Added animation + transition

--- sources/transitions/4/script.js
+++
@@ -1,5 +0,0 @@
-let container = document.querySelector('#container');
-container.addEventListener('click', function(e) {
-    // container.classList.toggle('rotate');
-    container.classList.toggle('scale');
-});
--- sources/transitions/6/script.js
+++
@@ -1,4 +0,0 @@
-// let container = document.querySelector('#container');
-// container.addEventListener('click', function(e) {
-//     container.classList.toggle('scale');
-// });
